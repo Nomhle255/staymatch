15f16bd8d30b3a85de10d71304ce5927d74bd2ca
Nomhle255 <nomhle.cathala@africacode.academy>
2026-09-09 05:27:50 +0200

    feat: configure Prisma and authentication

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,26 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const connectionString = process.env.DATABASE_URL;
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined");
+}
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+const adapter = new PrismaPg({
+  connectionString,
+});
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
